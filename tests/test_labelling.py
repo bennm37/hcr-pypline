5edd59bbc407baad4720d4da1c1861b9513a865e
Anqi <anqi.huang.haq@gmail.com>
2024-05-17 11:08:57 +0100

    Filename

--- tests/test_labelling.py
+++ tests/test_labelling.py
@@ -2,18 +2,19 @@ from hcrp.labelling import get_spline, get_contour
 import numpy as np
 from skimage.io import imread
 
-dropbox_root = "/Users/huanga/The Francis Crick Dropbox"
+dropbox_root = "/Users/huanga/The Francis Crick Dropbox/VincentJ/Anqi/Intership/AI_segmentation/python_segmentation/"
+filename = "TdEmbryo_Hoechst_pMad488_dpp546_brk647_20240506_LimbExtension10-Ant"
 def test_get_spline():
-    stack = np.array(imread(f'{dropbox_root}/VincentJ/Anqi/Intership/AI_segmentation/python_segmentation/TdEmbryo_Hoechst_pMad488_dpp546_brk647_20240506_LimbExtension10-Ant.tif'))
+    stack = np.array(imread(f'{dropbox_root}{filename}.tif'))
     layer = 26
     img = stack[layer, :, :, 3]
-    get_spline(img, 'test_spline')
+    get_spline(img, f'{filename}')
 
 def test_get_contour():
-    stack = np.array(imread(f'{dropbox_root}/VincentJ/Anqi/Intership/AI_segmentation/python_segmentation/TdEmbryo_Hoechst_pMad488_dpp546_brk647_20240506_LimbExtension10-Ant.tif'))
+    stack = np.array(imread(f'{dropbox_root}{filename}.tif'))
     layer = 26
     img = stack[layer, :, :, 3]
-    get_contour(img, 'test_contour')
+    get_contour(img, f'{filename}')
 
 
 if __name__ == '__main__':
