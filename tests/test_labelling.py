420c82bf5886001ec1d6909cf0ab911ae3c1fcef
Ben Mindlin <bennm37@gmail.com>
2024-05-17 11:01:38 +0100

    Window Name Refactor

--- tests/test_labelling.py
+++ tests/test_labelling.py
@@ -1,4 +1,4 @@
-from hcrp.labelling import get_spline, get_contour
+from hcrp.labelling import get_spline, get_contour, get_mean_region
 import numpy as np
 from skimage.io import imread
 
@@ -14,7 +14,14 @@ def test_get_contour():
     img = stack[layer, :, :, 3]
     get_contour(img, 'test_contour')
 
+def test_get_mean_region():
+    stack = np.array(imread('data/TdEmbryo_Hoechst_pMad488_dpp546_brk647_20240506_LimbExtension10-Ant.tif'))
+    layer = 26
+    img = stack[layer, :, :, 3]
+    get_mean_region(img, None, 'Background', size=50, vmax=None)
+
 
 if __name__ == '__main__':
-    # test_get_spline()
+    test_get_spline()
     test_get_contour()
+    test_get_mean_region()
